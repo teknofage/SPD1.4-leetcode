19899be84f7a79dfaa03057cc0c32c0fc5467ba7
teknofage <teknofage@gmail.com>
2020-04-06 03:01:01 -0700

    first problem solved

--- leet1.py
+++ leet1.py
@@ -10,12 +10,39 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1]."""
 
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
-    }
-};
-
 given_nums = [2, 7, 11, 15]
+target = 9
+
+# Brute force way
+def brute_force(given_nums, target):
+    for i in range(len(given_nums)-1): #for each index in the length of the array
+        for j in range(i+1, len(given_nums)): #for each index in the array after the index i
+            if given_nums[i] + given_nums[j] == target: #if the two indices add up to the target
+                print(given_nums[i], given_nums[j]) #print the two numbers at those indices
+                return True
+    return False #if no combination found, return False
+
+print(brute_force(given_nums, target)) #print the function result
+
+
+# Hash table way
+#i = 0 #first element in hash table
+#hash = dict() #create a dictionary
+#hash[7] = 2 #first element in hash table is 2, difference between it and target is 7
+
+#i = 1 #second element in hash table
+#hash[2] = 7 #second element in hash table is 7, difference between it and target is 2
+
+def hash_table(given_nums, target):
+    hash = dict() #create a dictionary
+    for i in range(len(given_nums)): #for each index in the range of the array
+        if given_nums[i] in hash: #if the element is in the hash table
+            print(hash[given_nums[i]], given_nums[i]) #print the corresponding indices from the hash table and the array
+            return True 
+        else:
+            hash[target - given_nums[i]] = given_nums[i] #else if the element is not present in the hash table, subtract it from the target
+    return False #if combination is not found
+    
+print(hash_table(given_nums, target))
+                 
+        
--- leet1readme.md
+++ leet1readme.md
@@ -1,19 +1,32 @@
 Restate the Problem 
+* There is an array.
+* it contains an unspecified number of integers
+* two of the numbers add up to a specific target
 
 Ask Clarifying Questions 
-* data types
-* edge cases
+* data types - is there only one data type?
+* edge cases - are there negative numbers? floats? repeated numbers?
+* Do the indices need to be returned in the order they appear in the array?
 
 State Assumptions 
+* each input has exactly one solution
+* the array contains only integers
+* the target is an integer
 
 Think Out Loud 
 
 Brainstorm Solutions 
+* The brute force solution would be to search for all possible pairs of numbers - sort the array in increasing order, initialize two indices and test whether they add up to the target. loop through numbers at indices until a combination is found which sums to the target
+* An alternative solution would be to sort the integers into a hash table, from which they would be quicker to search
 
 Explain Rationale 
+* Using the hash table would get us to the answer faster so I'm going to try that
+* Hash Tables are commonly used as look-up table data structures because they are really fast and reliable when storing key-value pairs where quick look-up is required.
 
 Discuss tradeoffs 
+* The brute force solution would take longer, but also take up less space - time complexity: O(n^2), space complexity: O(1)
+* The hashtable solution would have a shorter time complexity, but use up more space.
 
 Suggest Improvements 
-* edge cases
+* edge cases - 
 * other sides of the problem
