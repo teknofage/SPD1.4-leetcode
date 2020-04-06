d9aee568be5ba164c5df07715ff9766de4eaf466
teknofage <teknofage@gmail.com>
2020-04-06 03:49:12 -0700

    second problem started

---
+++ leet2.py
@@ -0,0 +1,37 @@
+"""
+https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+Convert Binary Number in a Linked List to Integer
+
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+
+Example 1:
+
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+
+Constraints:
+
+The Linked List is not empty.
+Number of nodes will not exceed 30.
+Each node's value is either 0 or 1.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getDecimalValue(self, head):
+        """
+        :type head: ListNode
+        :rtype: int
+        """
+int("101", 2)
--- leet2readme.md
+++ leet2readme.md
@@ -1,10 +1,15 @@
 Restate the Problem 
+* there is a linked list
+* each node has the value of either 1 or 0
+* this linked list also represents a binary number
+* we need to return the converted decimal number of this binary number
 
 Ask Clarifying Questions 
 * data types
-* edge cases
+* edge cases - could the list be empty? NO Could it be really long? Not longer than 30 nodes
 
 State Assumptions 
+* each node has a value of either 1 or 0
 
 Think Out Loud 
 
