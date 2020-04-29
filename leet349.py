79ea6d1bd4f12d49bc074cc74c2ec3390e53cef9
teknofage <teknofage@gmail.com>
2020-04-28 22:54:44 -0700

    variable tables added for 349 and 350

--- leet349.py
+++ leet349.py
@@ -1,77 +1,77 @@
-class Solution:
-    def intersection(nums1, nums2):
-        
-        nums1 = [1,2,2,1]
-        nums2 = [2,2] 
-        expected_output = [2] 
-        
-        """given two arrays
-        return single instance of common elements"""
-        
-#       Solution1
-        #       return (set(nums1) & set(nums2))
-        
-        
-#       Solution2
-        """iterate through nums1 and add numbers to a hash table
-        iterate through nums2 and compare to hash table
-        add duplicates to new list, as long as that list does not 
-        already contain them"""
-        
-        # Variable Table
-        # Variable . Value
-        # nums1    . [1,2,2,1]
-        # nums2    . [2,2] 
-        # x1       . 1
-        # h_set    . (1)
-        # x2       . 2
-        # h_set    . (1,2)
-        # x3       . 2
-        # h_set    . (1,2,2)
-        # x4       . 1
-        # h_set    . (1,2,2,1)
-        # y1       . 2
-        # output   . [2]
-        # y2       . 2
-        # output   . [2]
-        
-        
-        h_set = set()
-        output = set()
-        for x in nums1:
-            h_set.add(x)
-        for y in nums2:
-            if y in h_set and y not in output:
-                output.add(y)
-                
-        print(list(output))
-        return list(output)
+
+def intersection(nums1, nums2):
     
+    nums1 = [1,2,2,1]
+    nums2 = [2,2] 
+    expected_output = [2] 
+    
+    """given two arrays
+    return single instance of common elements"""
+    
+#       Solution1
+    #       return (set(nums1) & set(nums2))
+    
+    
+#       Solution2
+    """iterate through nums1 and add numbers to a hash table
+    iterate through nums2 and compare to hash table
+    add duplicates to new list, as long as that list does not 
+    already contain them"""
+    
+    # Variable Table
+    # Variable   . Value
+    # nums1      . [1,2,2,1]
+    # nums2      . [2,2] 
+    # x1         . 1
+    #   h_set    . (1)
+    # x2         . 2
+    #   h_set    . (1,2)
+    # x3         . 2
+    #   h_set    . (1,2,2)
+    # x4         . 1
+    #   h_set    . (1,2,2,1)
+    # y1         . 2
+    #   output   . [2]
+    # y2         . 2
+    #   output   . [2]
+    
+    
+    h_set = set()
+    output = set()
+    for x in nums1:
+        h_set.add(x)
+    for y in nums2:
+        if y in h_set and y not in output:
+            output.add(y)
+            
+    print(list(output))
+    return list(output)
+
 #       Solution3
-        """search both lists 
-        return one instance of common elements"""
-        
-        # Variable Table
-        # Variable . Value
-        # nums1    . [1,2,2,1]
-        # nums2    . [2,2] 
-        # x1       . 1
-        # x2       . 2
-        # x3       . 2
-        # x4       . 1
-        # y1       . 2
-        # output   . [2]
-        # y2       . 2
-        # output   . [2]
-        
-        # output = set()
-        # for x in nums1:
-        #     for y in nums2:
-        #         if x == y:
-        #             output.add(x)
-        # return list(output)
-        
-        
+    """search both lists 
+    return one instance of common elements"""
+    
+    # Variable Table
+    # Variable . Value
+    # nums1    . [1,2,2,1]
+    # nums2    . [2,2] 
+    # x1       . 1
+    # x2       . 2
+    # x3       . 2
+    # x4       . 1
+    # y1       . 2
+    # output   . [2]
+    # y2       . 2
+    # output   . [2]
+    
+    # output = set()
+    # for x in nums1:
+    #     for y in nums2:
+    #         if x == y:
+    #             output.add(x)
+    # return list(output)
+    
+    
 #       Solution4
 #        edit one list by removing duplicates and numbers 
 #       on in both lists until it is the output
@@ -83,4 +83,7 @@ class Solution:
 #         for x in nums1:
 #             for y in nums2:
 
-    intersection([1,2,2,1], [2,2])
+intersection([1,2,2,1], [2,2])
+
+if __name__ == '__main__':
+    assert intersection([1,2,2,1], [2,2]) == [2]
--- leet350.py
+++ leet350.py
@@ -1,17 +1,52 @@
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        nums2.sort()
-        pointer1 = 0
-        pointer2 = 0
-        output = []
-        while pointer1 < len(nums1) and pointer2 < len(nums2):
-            if nums1[pointer1] == nums2[pointer2]:
-                output.add(nums1[pointer1])
-                pointer1 += 1
-                pointer2 += 1
-            elif nums1[pointer1] < nums2[pointer2]:
-                pointer1 
-            
-                
-        return list(output)
+
+def intersect(nums1, nums2):
+    
+    nums1 = [4,9,5] 
+    nums2 = [9,4,9,8,4]
+    expected_output = [4,9]
+    
+    # Variable Table
+    # Variable . Value
+    # nums1          . [4,9,5]
+    # nums2          . [9,4,9,8,4] 
+    # nums1 sorted   . [4,5,9]
+    # nums2 sorted   . [4,4,8,9,9] 
+    # pointer1       . 4
+    #   pointer2     . 4
+    #       output   . [4]
+    # pointer1       . 5
+    #   pointer2     . 4
+    #       output   . [4]
+    # pointer1       . 5
+    #   pointer2     . 8
+    #       output   . [4]
+    # pointer1       . 9
+    #   pointer2     . 8
+    #       output   . [4]
+    # pointer1       . 9
+    #   pointer2     . 9
+    #       output   . [4, 9]
+    
+    
+    nums1.sort()
+    nums2.sort()
+    pointer1 = 0
+    pointer2 = 0
+    output = []
+    while pointer1 < len(nums1) and pointer2 < len(nums2):
+        if nums1[pointer1] == nums2[pointer2]:
+            output.append(nums1[pointer1])
+            pointer1 += 1
+            pointer2 += 1
+        elif nums1[pointer1] < nums2[pointer2]:
+            pointer1 += 1
+        else:
+            pointer2 += 1
+        
+    print(list(output))
+    return list(output)
+
+intersect([4,9,5], [9,4,9,8,4])
+
+if __name__ == '__main__':
+    assert intersect([4,9,5], [9,4,9,8,4]) == [4,9]
